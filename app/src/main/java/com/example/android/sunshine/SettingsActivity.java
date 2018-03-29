be61f56ca40e1f2c93f2c17dde3a503429e8e756
dashan124 <dashanjotsingh961999@gmail.com>
2018-03-29 17:52:54 +0530

    some error while settings clicked

--- app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -5,13 +5,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_settings);
         this.setContentView(R.layout.activity_settings);
         try {
             this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -19,6 +19,7 @@ public class SettingsActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
