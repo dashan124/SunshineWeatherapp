a78b142a47006f507dcf5832e584f247ef563e44
dashan124 <dashanjotsingh961999@gmail.com>
2018-03-29 13:51:51 +0530

    first commit

---
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORCAST_SHARE_HASHTAG="#Sunshine";
+    private String mForecast;
+    private TextView mWeatherDisplay;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mWeatherDisplay=(TextView) findViewById(R.id.tv_display_weather);
+        Intent intentThatStartedThisActivity=getIntent();
+        if(intentThatStartedThisActivity!=null){
+            if(intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)){
+                mForecast=intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+
+        }
+    }
+}
