047c0054ba312c7f1e8794563c762d36b9cabcbc
dashan124 <dashanjotsingh961999@gmail.com>
2018-03-30 11:03:38 +0530

    table created

--- app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class WeatherDbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME="weather.db";
-    private static final int DATABASE_VERSION =1;
+    private static final int DATABASE_VERSION =2;
     public WeatherDbHelper(Context context){
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
     }
